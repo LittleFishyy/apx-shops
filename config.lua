1f2369e184d92dda3cf9c3679242a98b6d61a7fa
KizzyyyDev <gatodevgames@gmail.com>
2021-09-06 06:10:39 +0200

    Fixed minor bug in to blip.

--- config.lua
+++ config.lua
@@ -5,23 +5,23 @@ Config.Shops = {
         name = 'Badulake Central',
         blipid = 59,
         blipcolor = 0,
-        blipsize = 0.8,
+        blipsize = 0.4,
         coords = vec3(-708.4631, -920.2416, 19.01392),
         items = {
             {label = "Pan", name = "bread", price = 90},
             {label = "Agua", name = "water", price = 90}
         }
     },
-    -- { -- Ejemplo
-    --     name = 'Licoreria',
-    --     blipid = 93, -- Blip ID (( https://docs.fivem.net/docs/game-references/blips/ ))
-    --     blipcolor = 0, -- Blip Color (( https://wiki.gtanet.work/index.php?title=Blips ))
-    --     blipsize = 0.8, -- Blip Scale
-    --     coords = vec3(-708.4631, -920.2416, 19.01392),
-    --     items = {
-    --         {label = "Pan", name = "bread", price = 90},
-    --         {label = "Agua", name = "water", price = 90}
-    --     }
-    -- },
+    { -- Ejemplo
+        name = 'Licoreria',
+        blipid = 93, -- Blip ID (( https://docs.fivem.net/docs/game-references/blips/ ))
+        blipcolor = 0, -- Blip Color (( https://wiki.gtanet.work/index.php?title=Blips ))
+        blipsize = 0.4, -- Blip Scale
+        coords = vec3(-708.4631, -935.2416, 19.01392),
+        items = {
+            {label = "Pan", name = "bread", price = 90},
+            {label = "Agua", name = "water", price = 90}
+        }
+    },
 }
 
